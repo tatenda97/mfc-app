e4f48f71b7d57f8766a2f4d1000f0bd4cdc6a7fa
kudakwashe <kuda@nightsbridge.co.za>
2021-01-09 15:09:09 +0200

    Added api controller and solved issues

--- src/farmer-equipment/farmer-equipment.controller.ts
+++ src/farmer-equipment/farmer-equipment.controller.ts
@@ -1,4 +1,61 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { FarmerEquipmentService } from './farmer-equipment.service';
+import { CreateFarmerEquipmentDTO } from './dto/farmer.equipment.dto';
+import { FarmerEquipment } from './farmer.equipment.entity';
 
 @Controller('farmer-equipment')
-export class FarmerEquipmentController {}
+export class FarmerEquipmentController {
+  constructor(private equipmentService: FarmerEquipmentService) {}
+
+  @Post('create')
+  public async createFarmAsset(
+    @Body() createFarmerEquipmentDto: CreateFarmerEquipmentDTO,
+  ): Promise<FarmerEquipment> {
+    const farmerEquipment = await this.equipmentService.createEquipment(
+      createFarmerEquipmentDto,
+    );
+    return farmerEquipment;
+  }
+
+  @Get('all')
+  public async getFarmAsset(): Promise<FarmerEquipment[]> {
+    const equipment = await this.equipmentService.getFarmerEquipments();
+    return equipment;
+  }
+
+  @Get('/:equipmentId')
+  public async getFarmer(@Param('equipmentId') equipmentId: number) {
+    const farmAsset = await this.equipmentService.getFarmerEquipment(
+      equipmentId,
+    );
+    return farmAsset;
+  }
+
+  @Patch('/edit/:equipmentId')
+  public async editContractForm(
+    @Body() createFarmerEquipmentDto: CreateFarmerEquipmentDTO,
+    @Param('equipmentId') equipmentId: number,
+  ): Promise<FarmerEquipment> {
+    const equipment = await this.equipmentService.editEquipment(
+      equipmentId,
+      createFarmerEquipmentDto,
+    );
+    return equipment;
+  }
+
+  @Delete('/delete/:equipmentId')
+  public async deleteEquimentId(@Param('equipmentId') equipmentId: number) {
+    const deletedEquipment = await this.equipmentService.deleteEquipment(
+      equipmentId,
+    );
+    return deletedEquipment;
+  }
+}
